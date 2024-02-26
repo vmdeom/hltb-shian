7e5aaa2f54e84650f5d1e6b6698d450452a2d7af
vmdeom <vitormourao.dev@outlook.com>
2024-02-26 19:49:31 -0300

    add data manipulation

--- src/app.js
+++ src/app.js
@@ -10,8 +10,43 @@ const app = express();
 app.set("view-engine", "ejs");
 app.use(express.json());
 
+function readGameData(data){
+
+    let dataArr = []
+    
+    data.forEach(element => {
+        //console.log(`${JSON.stringify(element)} ---`);
+        let newElement = JSON.stringify(element);
+            dataArr.push(newElement);
+    });
+
+    return formatGameData(dataArr);
+}
+
+function formatGameData(data){
+
+    let relevantGameData = {
+        "name": "",
+        "gameplayMain": "",
+        "gameplayMainExtra": "",
+        "gameplayCompletionist": ""
+    }
+
+    let dataArr = []
+
+    data.forEach(element => {
+        relevantGameData.name = JSON.parse(element).name;
+        relevantGameData.gameplayMain = JSON.parse(element).gameplayMain;
+        relevantGameData.gameplayMainExtra = JSON.parse(element).gameplayMainExta;
+        relevantGameData.gameplayCompletionist = JSON.parse(element).gameplayCompletionist;
+        dataArr.push(relevantGameData)
+    })
+
+    return dataArr;
+}
+
 app.get('/', (req, res) => {
-    data = hltbService.search(req.query.game).then(result => res.send(result));
+    data = hltbService.search(req.query.game).then(result => console.log(readGameData(result)));
 })
 
 app.listen(PORT, () => {
