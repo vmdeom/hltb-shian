7e6a59affd9264b25ff2f5b0f0b994ae35d55453
vmdeom <vitormourao.dev@outlook.com>
2024-02-26 11:31:44 -0300

    add hltb service and env config file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const hltb = require("howlongtobeat");
+const hltbService = new hltb.HowLongToBeatService();
+const config = require("../env/prod.cfg")
+
+const HOST = config.host;
+const PORT = config.port;
+
 
 const app = express();
 app.set("view-engine", "ejs");
 app.use(express.json());
 
-app.listen(3000 ,"localhost", () => {
-    console.log("listening");
+app.get('/', (req, res) => {
+    hltbService.search(req.query.game).then(result => console.log(result));
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Listening at ${HOST}:${PORT}`);
 });
